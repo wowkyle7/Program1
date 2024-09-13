bcdad973995f5314ada06c62816f3db3765a1de8
“wowkyle7” <kjschultz42@tntech.edu>
2024-09-13 16:39:32 -0500

    Created program files

---
+++ DataClass.cpp
@@ -0,0 +1,3 @@
+//  Stores some kind of information
+//  Data is private but midifiable through function calls
+//  Contains at least one pointer
---
+++ OtherClass.cpp
@@ -0,0 +1,4 @@
+//  Is either top-level or bottom-level
+//      can be used as a median between the driver and storage
+//      can be used as a sub data in the class
+//  contains at least one pointer
