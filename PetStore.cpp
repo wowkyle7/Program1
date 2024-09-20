d3978d2055c99efc1586d0b60388138025094cbf
“wowkyle7” <kjschultz42@tntech.edu>
2024-09-20 14:56:22 -0500

    Renamed Files & created storage class

---
+++ PetStore.cpp
@@ -0,0 +1,12 @@
+//  Pointer to an Array of pointers to the data
+//  Data stored is the data class
+//      ex. parking lot, toy box, etc
+//      requirements: be able to read items from a file, be able to print the contents to the screen
+
+class PetStore{
+    public:
+    
+
+
+
+};
